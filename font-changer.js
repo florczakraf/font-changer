0b62c24b50f55cea89440917e2bc24eaac446363
Rafał Florczak <florczak.raf@gmail.com>
2018-05-21 21:16:28 +0200

    Inject css via manifest

--- font-changer.js
+++ font-changer.js
@@ -78,62 +78,6 @@ function stopSelection() {
     document.removeEventListener("click", clickHandler);
 }
 
-function setStyles() {
-    const styles = document.createElement("style");
-    const rawStyles = `
-        .font-changer-popup * {
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-        }
-
-        .font-changer-popup {
-            position: fixed;
-            width: 600px;
-            max-height: 400px;
-            left: 5px;
-            top: 5px;
-            border-top: 1px solid #e7e7e7;
-            box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
-            background-color: white;
-            font-family: sans-serif;
-            z-index: 9999;
-        }
-
-        .font-changer-list {
-            overflow-y: scroll;
-            width: 100%;
-            max-height: 300px;
-            list-style: none;
-
-        }
-
-        .font-changer-list-element {
-            border-bottom: 1px solid #e7e7e7;
-            background-color: white;
-            width: 100%;
-            padding: 5px;
-            cursor: pointer;
-            display: flex;
-        }
-
-        .font-changer-list-element:hover {
-            background-color: #bababa;
-        }
-
-        .font-changer-element-classes {
-            font-size: 70%;
-            width: 100%;
-            white-space: nowrap;
-            overflow: hidden;
-            text-overflow: ellipsis;
-            margin-left: 10px;
-        }
-    `;
-    styles.innerHTML = rawStyles;
-    document.body.appendChild(styles);
-}
-
 chrome.runtime.onMessage.addListener((message) => {
     if (message.command === "startSelection") {
         startSelection();
@@ -142,4 +86,3 @@ chrome.runtime.onMessage.addListener((message) => {
         console.error(`Unknown message: ${JSON.stringify(message)}`);
     }
 });
-setStyles();
